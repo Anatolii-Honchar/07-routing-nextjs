e7757dc1622907b66a0b3ae38db6921d7c40f4c2
Anatolii Honchar <gonchar_tol@ukr.net>
2026-04-05 19:11:08 +0300

    Implement error handling for note fetching and add NotFound component

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,19 +5,28 @@ import {
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
 export default async function NoteDetailsPage({ params }: Props) {
-  const { id } = await params; // ✅ РОЗПАКУВАЛИ
+  const { id } = await params;
 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
+    queryFn: async () => {
+      try {
+        const note = await fetchNoteById(id);
+        return note;
+      } catch (error) {
+        // 👇 якщо нотатку не знайдено → 404
+        notFound();
+      }
+    },
   });
 
   return (
